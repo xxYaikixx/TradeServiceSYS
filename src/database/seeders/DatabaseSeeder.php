c6ec5eecc9f4f964be69998d71a1d50e718be7ff
xxYaikixx <yuka.aiki@banana-877.com>
2022-09-24 15:32:52 +0900

    Userデータ入れ直し

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserTableSeeder::class,
-            ItemTableSeeder::class,
-            TradeInfoTableSeeder::class,
+            // ItemTableSeeder::class,
+            // TradeInfoTableSeeder::class,
         ]);
     }
 }
