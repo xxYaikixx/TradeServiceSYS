86fb3fd1f8ed09f4d31d37df813975a976aeb4a9
yaiki877 <yuka.aiki@banana-877.com>
2023-01-24 16:30:54 +0900

    Chakra UI + React Hook Formのガイドに沿って書き方を変更

--- src/resources/js/components/Register.jsx
+++ src/resources/js/components/Register.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, ChakraProvider, FormControl, FormLabel, Heading, Input, Stack, Text, useDisclosure, Container, FormErrorMessage } from '@chakra-ui/react';
+import { Box, Button, ChakraProvider, FormControl, FormLabel, Input, Stack, useDisclosure, Container, FormErrorMessage } from '@chakra-ui/react';
 import { Header } from './Header';
-import axios from 'axios';
 import { usePostalJp } from 'use-postal-jp';
 import { useForm } from 'react-hook-form';
-import { RegisterConfirm } from './RegisterConfirm';
-import { registerStyles } from '@emotion/utils';
+
 
 export const Register = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -31,16 +29,6 @@ export const Register = () => {
         error_list: [],
     });
 
-    // useEffect(() => {
-    //     setFormData({ ...formData, address: address, zipcode: zipcode });
-    // }, [address])
-
-
-    // const handleInput = (e) => {
-    //     e.persist();
-    //     setFormData({ ...formData, [e.target.name]: e.target.value });
-    // }
-
     const {
         getValues,
         handleSubmit,
@@ -48,8 +36,7 @@ export const Register = () => {
         formState: { errors, isSubmitting },
     } = useForm()
 
-    function onSubmit(values) {
-        console.log("submit");
+    function onSubmit() {
         return navigate('/register/confirm', { state: getValues() });
     }
 
@@ -57,92 +44,128 @@ export const Register = () => {
         <>
             <ChakraProvider>
                 <Header btnRef={btnRef} onOpen={onOpen} isOpen={isOpen} onClose={onClose} />
-                <Box my={5} >
-                    <Container minWidth='max-content' borderWidth='1px' borderRadius='lg' alignContent='center' align="center" p={10}>
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <FormControl isInvalid={errors.name}>
-                                <FormLabel htmlFor='name'>氏名</FormLabel>
-                                <Input
-                                    id='name'
-                                    placeholder='氏名'
-                                    {...register('name', {
-                                        required: '氏名を入力してください',
-                                    })}
-                                />
-                                <FormErrorMessage>
-                                    {errors.name && errors.name.message}
-                                </FormErrorMessage>
-                            </FormControl>
-                            <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
-                                確認
-                            </Button>
-                        </form>
-                    </Container>
-                </Box>
-            </ChakraProvider >
-            {/* <Box my={5} >
+                <Stack spacing={5}>
+                    <Box my={5} >
                         <Container minWidth='max-content' borderWidth='1px' borderRadius='lg' alignContent='center' align="center" p={10}>
-                            <Heading as='h2' size='xl' align='center' my={5} color='gray.700'>会員登録</Heading>
-                            <Stack spacing={5}>
-                                <Box>
-                                    <FormLabel>氏名</FormLabel>
-                                    <Input type='text' name="name" {...register("name")} />
-                                    {/* <Input type='text' name="name" onChange={handleInput} value={formData.name} {...register("simei")} /> */}
-            {/* <span><Text fontSize='sm' color='red' align='left'>{formData.error_list.name}</Text></span> */}
-            {/* </Box>
-            <Box>
-                <FormLabel>表示名</FormLabel>
-                <Input type='text' name="nickname" {...register("nickname")} />
-                <span><Text fontSize='sm' color='red' align='left'>{formData.error_list.nickname}</Text></span>
-            </Box>
-            <FormLabel>サムネイル</FormLabel> */}
-            {/* <Input
-                                id={previewUrl}
-                                ref={inputRef}
-                                name={previewUrl}
-                                type="file"
-                                accept="image/*"
-                                onChange={onFileInputChange}
-                            /> */}
-            {/* <Input type="file" onChange={(e) => {
-                                    inputChange("image", e.target.files[0]);
-                                }} accept="image/*" multiple ref={inputRef} name="image" /> */}
-            {/* <Box>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <FormControl isInvalid={errors.name}>
+                                    <FormLabel htmlFor='name'>氏名</FormLabel>
+                                    <Input
+                                        id='name'
+                                        placeholder='氏名'
+                                        {...register('name', {
+                                            required: '氏名を入力してください',
+                                        })}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.name && errors.name.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <FormControl isInvalid={errors.nickname}>
+                                    <FormLabel>表示名</FormLabel>
+                                    <Input
+                                        id='nickname'
+                                        placeholder='表示名'
+                                        {...register("nickname", {
+                                            required: '表示名を入力してください',
+                                        })} />
+                                    <FormErrorMessage>
+                                        {errors.nickname && errors.nickname.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <FormLabel>サムネイル</FormLabel>
+                                <FormControl isInvalid={errors.email}>
                                     <FormLabel>メールアドレス</FormLabel>
-                                    <Input type='text' name="email" {...register("email")} />
-                                    <span><Text fontSize='sm' color='red' align='left'>{formData.error_list.email}</Text></span>
-                                </Box>
-                                <Box>
+                                    <Input
+                                        id='email'
+                                        placeholder='メールアドレス'
+                                        {...register("email", {
+                                            required: 'メールアドレスを入力してください',
+                                        })} />
+                                    <FormErrorMessage>
+                                        {errors.email && errors.email.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <FormControl isInvalid={errors.zipcode}>
                                     <FormLabel>郵便番号</FormLabel>
-                                    <Input type='text' name="zipcode" {...register("zipcode")} onChange={(e) => setZipcode(e.target.value)} />
-                                    <span><Text fontSize='sm' color='red' align='left'>{formData.error_list.zipcode}</Text></span>
-                                </Box>
-                                <Box>
+                                    <Input
+                                        id='zipcode'
+                                        placeholder='郵便番号'
+                                        {...register("zipcode", {
+                                            required: '郵便番号を入力してください',
+                                        })}
+                                        onChange={(e) => setZipcode(e.target.value)}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.zipcode && errors.zipcode.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <FormControl isInvalid={errors.address}>
                                     <FormLabel>住所</FormLabel>
-                                    <Input isReadOnly={true} value={loading || !address ? '' : address.prefecture + address.address1 + address.address2 + address.address3 + address.address4} {...register("address")} />
-                                    <span><Text fontSize='sm' color='red' align='left'>{formData.error_list.address}</Text></span>
-                                </Box> */}
-            {/* <Box>
-                                    <FormLabel>住所(続き)</FormLabel>
-                                    <Input type='text' name="address2" {...register("address2")} />
-                                    <span><Text fontSize='sm' color='red' align='left'>{formData.error_list.address2}</Text></span>
-                                </Box>
-                                <Box>
+                                    <Input isReadOnly={true}
+                                        id='address'
+                                        placeholder='住所'
+                                        value={zipcode.length < 7 || loading || !address ? '' : address.prefecture + address.address1 + address.address2 + address.address3 + address.address4}
+                                        {...register("address",
+                                            {
+                                                required: '住所を入力してください',
+                                            })} />
+                                    <FormErrorMessage>
+                                        {errors.address && errors.address.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <FormControl isInvalid={errors.address2}>
+                                    <FormLabel>住所（続き）</FormLabel>
+                                    <Input
+                                        id='address2'
+                                        placeholder='住所（続き）'
+                                        {...register("address2",
+                                            {
+                                                required: '住所（続き）を入力してください',
+                                            })} />
+                                    <FormErrorMessage>
+                                        {errors.address2 && errors.address2.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <FormControl isInvalid={errors.password}>
                                     <FormLabel>パスワード</FormLabel>
-                                    <Input type='password' name="password"
-                                        {...register("password")} />
-                                    <span><Text fontSize='sm' color='red' align='left'>{formData.error_list.password}</Text></span>
-                                </Box>
-                                <Box>
-                                    <FormLabel>パスワード(確認)</FormLabel>
-                                    <Input type='password' name="password2"
-                                        {...register("password2")} />
-                                    <span><Text fontSize='sm' color='red' align='left'>{formData.error_list.password2}</Text></span>
-                                </Box>
-                                <Button colorScheme='blue' onClick={() => {
-                                    return navigate('/register/confirm', { state: getValues() });
-                                }}>確認</Button>
-                            </Stack>*/}
+                                    <Input
+                                        type='password'
+                                        id='password'
+                                        placeholder='パスワード'
+                                        {...register("password", {
+                                            required: 'パスワードを入力してください',
+                                        })} />
+                                    <FormErrorMessage>
+                                        {errors.password && errors.password.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <FormControl isInvalid={errors.password2}>
+                                    <FormLabel>パスワード（確認）</FormLabel>
+                                    <Input
+                                        type='password'
+                                        id='password2'
+                                        placeholder='パスワード（確認）'
+                                        {...register("password2", {
+                                            validate: (value) => {
+                                                return (
+                                                    value === getValues("password") || "パスワードが一致しません"
+                                                );
+                                            }
+                                        })}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.password2 && errors.password2.message}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                                    確認
+                                </Button>
+                            </form>
+                        </Container>
+                    </Box>
+                </Stack>
+            </ChakraProvider >
         </>
     );
 }
--- src/resources/js/components/RegisterConfirm.jsx
+++ src/resources/js/components/RegisterConfirm.jsx
@@ -6,19 +6,17 @@ import { useLocation } from 'react-router-dom';
 
 export const RegisterConfirm = () => {
     const location = useLocation();
-    console.log("aaa");
-    console.log(location.state.name);
     return (
         <>
             <h2>確認画面</h2>
             <Box>
                 <p>氏名：{location.state.name}</p>
-                {/*<p>表示名：{location.state.nickname}</p> */}
-                {/* <p>メールアドレス：{location.state.email}</p> */}
-                {/* <p>郵便番号：{location.state.zipcode}</p>
+                <p>表示名：{location.state.nickname}</p>
+                <p>メールアドレス：{location.state.email}</p>
+                <p>郵便番号：{location.state.zipcode}</p>
                 <p>住所：{location.state.address}</p>
                 <p>住所（続き）：{location.state.address2}</p>
-                <p>パスワード：{location.state.password}</p> */}
+                <p>パスワード：{location.state.password}</p>
                 <Button value='登録' />
             </Box>
         </>
